d3f3a7bc331ac1f12f8437cb9d34d9a449a4559f
MrDewmon69 <149539322+MrDewmon69@users.noreply.github.com>
2024-06-20 22:36:43 +1200

    started to change the command handler around

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const { Client, IntentsBitField, ActivityType } = require('discord.js');
+const eventHandler = require('./handlers/eventHandler');
 require('dotenv').config()
 
 const client = new Client({
@@ -10,29 +11,6 @@ const client = new Client({
     ],
 });
 
-client.on('ready', (c) => {
-    console.log(`${c.user.username} is online!`);
-
-    client.user.setActivity({
-        name: "Life",
-        type: ActivityType.Streaming,
-        url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-    });
-});
-
-client.on('interactionCreate', (interaction) => {
-    if (!interaction.isChatInputCommand()) return;
-
-    if (interaction.commandName === 'hey') {
-        interaction.reply('Hey!')
-    }
-
-    if (interaction.commandName === 'add') {
-        const num1 = interaction.options.get('first-number')?.value;
-        const num2 = interaction.options.get('second-number')?.value;
-
-        interaction.reply(`The sum is ${num1 + num2}`)
-    }
-})
+eventHandler(client);
 
 client.login(process.env.bot_token)
